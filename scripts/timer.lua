6bbce547103b6fbfe6b137a3aeb1af9db5fe895e
Joao Moreira <joaomarcello.mm@gmail.com>
2023-01-29 09:37:26 -0300

    tudo certo com o timer

--- scripts/timer.lua
+++ scripts/timer.lua
@@ -19,15 +19,17 @@ end
 ---@param Game GameState.Game
 function Timer:__constructor__(Game, args)
     self.game = Game
-    self.time = args.init_time or 60
+    self.time = args.init_time or 80
+    self.time_init = self.time
     self.speed = 0.5
     self.acumulator = 0.0
     self.time_warning = 50
 
-    self.radius = args.radius or (32)
+    self.radius = args.radius or (36)
     local l, t, r, b = Game.camera:get_viewport_in_world_coord()
-    self.x = args.x or (r - self.radius - 20)
+    self.x = args.x or (r - self.radius - 32)
     self.y = args.y or (t + self.radius + 20)
+
     self:set_color2(0.2, 0.2, 0.2, 0.8)
     self.actives_eff = {}
 end
@@ -41,11 +43,25 @@ function Timer:update(dt)
         self.time = Utils:clamp(self.time - 1, 0, math.huge)
     end
 
-    if self.time == self.time_warning
+    if (self.time % 50 == 0 or self.time == self.time_warning
+        or self.time == 15)
         and not self.actives_eff["pulse"]
+        and self.time ~= self.time_init
     then
-        self.actives_eff['pulse'] = self:apply_effect("pulse", { max_sequence = 5, speed = 0.5 })
-        self.actives_eff['pulse']:set_final_action(function() self.actives_eff['pulse'] = false end)
+        local cycles
+        if self.time > 15 then
+            cycles = 5
+            if self.time > self.time_warning then
+                self.time_capture = self.time
+            end
+        end
+
+        self.actives_eff['pulse'] = self:apply_effect("pulse", { max_sequence = cycles, speed = 0.5, range = 0.1 })
+
+        self.actives_eff['pulse']:set_final_action(function()
+            self.time_capture = nil
+            self.actives_eff['pulse'] = false
+        end)
     end
 end
 
@@ -54,25 +70,34 @@ function Timer:draw_number(color, offset)
     offset = offset or 0
 
     Font.current:push()
-    Font.current:set_font_size(22)
+    Font.current:set_font_size(25)
 
-    local obj = Font:get_phrase(color .. self.time,
+    local time = self.time_capture or self.time
+
+    local obj = Font:get_phrase(color .. time,
         self.x, self.y, "left",
         math.huge
     )
     local w = obj:width()
-    obj:draw(self.x - w * 0.5 + offset - 1, self.y - 12 + offset, "left")
+    obj:draw(self.x - w * 0.5 + offset - 1, self.y - Font.current.__font_size * 0.5 + offset - 1, "left")
     Font.current:pop()
 end
 
-function Timer:draw()
+function Timer:draw_circle()
     love.graphics.setColor(self.color)
     love.graphics.circle("fill", self.x, self.y, self.radius)
+end
 
-    Affectable.draw(self, self.draw_number, "<color, 0.6, 0.6, 0.6>", 1)
-    Affectable.draw(self, self.draw_number)
+function Timer:draw()
+    self:draw_circle()
 
-    Font:print(self.actives_eff['pulse'], 300, 100)
+    if self.time > self.time_warning then
+        Affectable.draw(self, self.draw_number, "<color, 0.6, 0.6, 0.6>", 1)
+        Affectable.draw(self, self.draw_number)
+    else
+        Affectable.draw(self, self.draw_number, "<color, 0, 0, 0>", 1)
+        Affectable.draw(self, self.draw_number, "<color, 1, 0.1, 0.1>")
+    end
 end
 
 return Timer
